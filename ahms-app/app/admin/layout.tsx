e23804806ea165919d13c92d036d9c9fb68810f0
Kesovannaream <kesovannaream18@gmail.com>
2026-05-16 23:00:01 +0700

    Fixed Khmer font

--- ahms-app/app/admin/layout.tsx
+++ ahms-app/app/admin/layout.tsx
@@ -1,11 +1,19 @@
-export default function AdminLayout({
+import './globals.css'
+import { Kantumruy_Pro } from 'next/font/google'
+
+const khmerFont = Kantumruy_Pro({ 
+  subsets: ['khmer'],
+  weight: ['400', '700'] 
+})
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <>
-      {children}
-    </>
-  );
+    <html lang="km">
+      <body className={khmerFont.className}>{children}</body>
+    </html>
+  )
 }
