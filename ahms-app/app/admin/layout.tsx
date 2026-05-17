4c9d124a7f8d96c1aff1757c39699fe4540caf46
Kesovannaream <kesovannaream18@gmail.com>
2026-05-17 13:01:44 +0700

    Fixed CSS paths in both layouts

--- ahms-app/app/admin/layout.tsx
+++ ahms-app/app/admin/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../globals.css'
 import { Kantumruy_Pro } from 'next/font/google'
 
 const khmerFont = Kantumruy_Pro({ 
--- ahms-app/app/layout.tsx
+++ ahms-app/app/layout.tsx
@@ -1,4 +1,4 @@
-import '../globals.css'
+import './globals.css'
 import { Kantumruy_Pro } from 'next/font/google'
 import Link from 'next/link'
 
